cd2687995aa6aadc5b4840c74252aebda85cfa23
Jose Luis Koller <joseluiskoller98@gmail.com>
2023-04-07 15:08:51 -0300

    ver 0.0.0.1

---
+++ scripts/content.js
@@ -0,0 +1,23 @@
+function quitarFormatoNumero(numeroConFormato) {
+  const strNumero = numeroConFormato.toString();
+  const numeroSinFormato = strNumero.replace(/\D/g, "");
+  const numero = Number(numeroSinFormato);
+  return numero;
+}
+
+async function logData() {
+  const response = await fetch(
+    "https://dolar-api-argentina.vercel.app/v1/dolares/blue"
+  );
+  const usd = await response.json();
+  let priceTag = document.querySelector(".andes-money-amount__fraction");
+  priceTag = quitarFormatoNumero(priceTag.innerHTML);
+  priceTag = priceTag / usd.venta;
+  const contenedor = document.querySelector(".ui-pdp-price__second-line");
+  const elemento = document.createElement("span");
+  elemento.className = "usd2ars";
+  elemento.innerHTML = `≈ U$D ${priceTag.toFixed(2)}`;
+  contenedor.appendChild(elemento);
+}
+
+logData();
