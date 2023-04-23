0525e8b9ae4b05bd15a5e7c78dbb41e8e22d323f
Jose Luis Koller <joseluiskoller98@gmail.com>
2023-04-23 16:47:26 -0300

    Error catch.

--- scripts/content.js
+++ scripts/content.js
@@ -53,32 +53,35 @@ async function logData() {
   let priceTag = document.querySelector(
     ".ui-pdp-price__second-line .andes-money-amount__fraction"
   );
-  priceTag = quitarFormatoNumero(priceTag.innerHTML);
-  priceTag = priceTag / usd.venta;
+  if (priceTag != null) {
+    priceTag = quitarFormatoNumero(priceTag.innerHTML);
+    priceTag = priceTag / usd.venta;
 
-  // Select container
-  const contenedor = document.querySelector(".ui-pdp-price__second-line");
+    // Select container
+    const contenedor = document.querySelector(".ui-pdp-price__second-line");
 
-  // Create element
-  const elemento = document.createElement("span");
-  elemento.classList.add("tooltip");
-  elemento.innerHTML = `≈ U$D ${priceTag.toFixed(2)}`;
-  contenedor.appendChild(elemento);
+    // Create element
+    const elemento = document.createElement("span");
+    elemento.classList.add("tooltip");
+    elemento.innerHTML = `≈ U$D ${priceTag.toFixed(2)}`;
+    contenedor.appendChild(elemento);
 
-  // Icono
-  const icon = document.createElement("div");
-  icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" class="bi bi-info-circle" viewBox="0 0 16 16">
+    // Icono
+    const icon = document.createElement("div");
+    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" class="bi bi-info-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
 </svg>`;
-  icon.classList.add("icon");
-  elemento.appendChild(icon);
+    icon.classList.add("icon");
+    elemento.appendChild(icon);
 
-  // Tooltip para saber el dolar que se toma
-  const tooltip = document.createElement("div");
-  tooltip.classList.add("tooltip-text");
-  tooltip.innerHTML = `$${usd.venta}`;
-  icon.appendChild(tooltip);
+    // Tooltip para saber el dolar que se toma
+    const tooltip = document.createElement("div");
+    tooltip.classList.add("tooltip-text");
+    tooltip.innerHTML = `$${usd.venta}`;
+    icon.appendChild(tooltip);
+  }
+
   let numero = obtenerMLA();
   if (numero != "null") {
     const responseUp = await fetch(
