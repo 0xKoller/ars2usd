c70dd4f9c9cadf12df8ebbd08c160ca4c8bf2480
Jose Luis Koller <joseluiskoller98@gmail.com>
2023-05-13 02:51:52 -0300

    Super update

--- popup/popup.js
+++ popup/popup.js
@@ -10,8 +10,6 @@ const saveOptions = async () => {
   chrome.tabs.reload();
 };
 
-document.getElementById("save").addEventListener("click", saveOptions);
-
 const getKeyName = async () => {
   const value = await chrome.storage.sync.get(["user_select"]).then((value) => {
     return value;
@@ -21,21 +19,36 @@ const getKeyName = async () => {
   return getKey;
 };
 
-const getText = (currency) => {
-  switch (currency) {
-    case "usd_blue":
-      return { color: "5A65C9", text: "Blue" };
-    case "usd_of":
-      return { color: "3A8842", text: "Oficial" };
-    case "usd_tur":
-      return { color: "D68235", text: "Turista" };
-  }
-};
-
-const span = document.getElementById("selected_currency");
-const a = getKeyName().then((e) => {
-  const values = getText(e);
-  span.innerHTML = values.text;
-  span.style.color = `#${values.color}`;
-  span.style.fontWeight = "bold";
+document.addEventListener("DOMContentLoaded", function () {
+  const select = document.getElementById("currency");
+  const setElements = getKeyName().then((e) => {
+    let blue = new Option("Blue", "usd_blue");
+    let oficial = new Option("Oficial", "usd_of");
+    let turista = new Option("Turista", "usd_tur");
+    // Este sistema de colores y demas, no es el mas optimos pero.. it gets the job done
+    const colores = {
+      usd_of: "#3a8842",
+      usd_blue: "#5a65c9",
+      usd_tur: "#d68235",
+    };
+    const elementos = Object.keys(colores);
+    select.appendChild(blue);
+    select.appendChild(oficial);
+    select.appendChild(turista);
+    for (let i = 0; i < select.options.length; i++) {
+      if (select.options[i].value === e) {
+        select.options[i].selected = true;
+        for (let z = 0; z < elementos.length; z++) {
+          console.log(elementos[z]);
+          if (elementos[z] === select.value) {
+            select.style.color = colores[elementos[z]];
+            break;
+          }
+        }
+        break;
+      }
+    }
+  });
 });
+
+document.getElementById("save").addEventListener("click", saveOptions);
